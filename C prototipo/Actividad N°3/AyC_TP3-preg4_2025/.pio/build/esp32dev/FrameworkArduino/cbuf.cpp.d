64277435562f7525af45e057a9ce9b33220578e3
vittoriodurigutti <vittodutti@gmail.com>
2025-04-18 19:46:57 -0300

    Se crean los modulos de conexion wifi, y mqtt. No se implementa nada en el codigo principal

--- C prototipo/Actividad N°3/AyC_TP3-preg4_2025/.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
+++ C prototipo/Actividad N°3/AyC_TP3-preg4_2025/.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
@@ -1,3 +1,3 @@
 .pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/EliteBook/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/EliteBook/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
+ C:/Users/Vitto\ Dell/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ C:/Users/Vitto\ Dell/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
